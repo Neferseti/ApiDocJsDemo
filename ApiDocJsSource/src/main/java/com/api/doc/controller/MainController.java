8b370bca967e112d207dea1b3869454e01a20ce6
ks_mani <kumar.mani017@gmail.com>
2017-08-23 11:22:19 +0530

    Added Api Spec notations

--- ApiDocJsSource/src/main/java/com/api/doc/controller/MainController.java
+++ ApiDocJsSource/src/main/java/com/api/doc/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.api.doc.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,38 @@ import com.google.gson.JsonObject;
 @RestController
 @RequestMapping("/user")
 public class MainController {
-
+	
+	/**
+	 * @api {get} /user?id={id} getUserById
+	 * @apiName GetUsersById
+	 * @apiGroup User
+	 *
+	 * @apiParam {Number} id Users unique ID.
+	 * 
+	 *
+	 * @apiSuccess {String} firstName First Name of the User.
+	 * @apiSuccess {String} middleName  Middle name of the User.
+	 * @apiSuccess {String} lastName  Last Name of the User.
+	 * @apiSuccess {Numeric} age  Age of the User.
+	 *
+	 * @apiSuccessExample Success-Response:
+	 *     HTTP/1.1 200 OK
+	 *     {
+	 *       "firstName": "John",
+	 *       "middleName": "M."
+	 *       "lastName": "Doe"
+	 *       "age":23
+	 *     }
+	 *
+	 * @apiError UserNotFound The id of the User was not found.
+	 *
+	 * @apiErrorExample Error-Response:
+	 *     HTTP/1.1 404 Not Found
+	 *     {
+	 *       "error": "UserNotFound"
+	 *     }
+	 */
+	
 	@RequestMapping(method=RequestMethod.GET)
 	public String getUsers(){
 		JsonObject response = new JsonObject();
@@ -22,6 +54,90 @@ public class MainController {
 		return new Gson().toJson(response);
 		
 		
+	}
+	
+	
+	/**
+	 * @api {post} /user/create createUser
+	 * @apiName CreateUser
+	 * @apiGroup User
+	 * 
+	 * @apiParam {String} firstName First Name of the User
+	 * @apiParam {String} middleName Middle Name of the User
+	 * @apiParam {String} lastName Last Name of the User
+	 * @apiParam {Numeric} age Age of the User
+	 * 
+	 * @apiExample Request Example
+	 * 	  {
+	 * 		"firstName":"Mani",
+	 * 		"middleName":"Kumar",
+	 * 		"lastName":"Srivastava",
+	 * 		"age":23
+	 * 	  }
+	 * 
+	 * @apiSuccess {String} message 
+	 * 
+	 * @apiSuccessExample Success-Response:
+	 *     HTTP/1.1 200 OK
+	 *     {
+	 *       "message":"User Created Successfully"
+	 *     }
+	 *     
+	 * @apiError CreateException Unable to create the user.
+	 *
+	 * @apiErrorExample Error-Response:
+	 *     HTTP/1.1 404 Not Found
+	 *     {
+	 *       "error": "Unable to create the user"
+	 *     }
+	 */
+	
+	@RequestMapping(value="/create", method=RequestMethod.POST)
+	public String createUser(@RequestBody String request){
+		return "User Created Successfully";
+	}
+	
+	/**
+	 * @api {put} /user/update?id={id} updateUser
+	 * @apiName UpdateUser
+	 * @apiGroup User
+	 * 
+	 * @apiParam {Numeric} _id id of the user to update
+	 * @apiParam {String} firstName First Name of the User
+	 * @apiParam {String} middleName Middle Name of the User
+	 * @apiParam {String} lastName Last Name of the User
+	 * @apiParam {Numeric} age Age of the User
+	 * 
+	 * @apiExample Request Example
+	 * 		curl -i /user/update?id=1
+	 * 		{
+	 * 			"firstName":"Mani",
+	 * 			"middleName":"Kumar",
+	 * 			"lastName":"Srivastava",
+	 * 			"age":23
+	 * 		}
+	 * 
+	 * @apiSuccess {String} message
+	 * 
+	 * @apiSuccessExample Success-Response
+	 * 
+	 * 	HTTP/1.1 200 OK
+	 * 	{
+	 * 		"message":"User Update Successfully"
+	 * 	}
+	 * 
+	 * @apiError UpdateException Unable to update the user
+	 * 
+	 * @apiErrorExample Error-Response
+	 * 
+	 * 	HTTP/1.1 404 Not Found
+	 * 	{
+	 * 		"error":"Unable to update the user"
+	 * 	}
+	 */
+	@RequestMapping(value="/update", method=RequestMethod.PUT)
+	public String updateUser(@RequestBody String request){
+		return "User Updated Successfully";
 	}
 	
 }
